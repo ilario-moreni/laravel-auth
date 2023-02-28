1e87b361d91098762dde8ef121a58629e35af841
ilario-moreni <ilario2001.mor@gmail.com>
2023-02-28 16:21:12 +0100

    initialize files

---
+++ resources/views/home.blade.php
@@ -0,0 +1,15 @@
+@extends('layouts.app')
+@section('content')
+    <main>
+        <div class="container">
+            <div class="row">
+                <div class="col">
+                    <div>
+                        <a href="#"></a>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+    </main>
+@endsection
